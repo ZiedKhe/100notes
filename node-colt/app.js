8af646700725a1981db0a517ca28dafd86ad0309
Zied Kheriji <zied.web@gmail.com>
2017-01-06 19:40:34 +0100

    adding Redis Session management

--- node-colt/app.js
+++ node-colt/app.js
@@ -1,8 +1,18 @@
 const path = require("path")
 const express = require("express");
+var RedisStore =  require('connect-redis')(express)
 const app = express();
 const ejs = require ('ejs')
 const port = 3000
+
+app.use(express.session({store: new RedisStore}));
+
+
+app.use(express.session({ store: new RedisStore({
+  host:'127.0.0.1',
+  port:6380,
+  prefix:'sess'
+}), secret: 'SEKR37' }));
 
 app.get("/exchange", function(req,res){
 	console.log("Exchange Shift");
