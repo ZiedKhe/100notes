9ad1b9f9408cfbfaae37ee7575585a6db59ce58c
Zied Kheriji <zied.web@gmail.com>
2017-01-09 12:38:09 +0100

    added POST logic - need bodyparser

--- node-colt/app.js
+++ node-colt/app.js
@@ -1,43 +1,55 @@
 const path = require("path")
 const express = require("express");
 // var RedisStore =  require('connect-redis')(express)
+//const bodyParser = require("body-parser");
 const app = express();
-const ejs = require ('ejs')
-const port = 3000
-
-// app.use(express.session({store: new RedisStore}));
+const ejs = require ('ejs');
+const port = 3000;
 
 
+// SESSION
 // app.use(express.session({ store: new RedisStore({
 //   host:'127.0.0.1',
 //   port:6380,
 //   prefix:'sess'
 // }), secret: 'SEKR37' }));
 
+
 // Serve the static content of "public" directory
 app.use(express.static('public'));
 
 
+// TEST DATA - to be removed
 var myShift = [
-	{number : 312, member : 98, shift: "6h-8h15", date : "15 mai 1980" },
-	{number : 313, member : 98, shift: "6h-8h15", date : "15 mai 1980" },
-	{number : 314, member : 98, shift: "6h-8h15", date : "15 mai 1980" }
+	{number : 312, member : 98, shift: "6h-8h15", date : "15 mai 1980", status : '' },
+	{number : 313, member : 98, shift: "6h-8h15", date : "15 mai 1980", status : 'reserved' },
+	{number : 314, member : 98, shift: "6h-8h15", date : "15 mai 1980", status : '' }
 	]
+
+var exchanges = [
+	{number : 312, member : 98, shift: "6h-8h15", date : "15 mai 1980", status : '' },
+	{number : 315, member : 930, shift: "6h-8h15", date : "15 mai 1980", status : '' },
+	{number : 334, member : 2341, shift: "6h-8h15", date : "15 mai 1980", status : '' },
+	{number : 335, member : 21, shift: "6h-8h15", date : "16 mai 1980", status : '' },
+	{number : 336, member : 2345, shift: "6h-8h15", date : "20 mai 1980", status : '' },
+	{number : 340, member : 221, shift: "6h-8h15", date : "22 mai 1980", status : '' }
+	];
+
+
+// ROUTES
 
 app.get("/exchange", function(req,res){
 	console.log("Exchange Shift");
 	res.render("exchange.ejs", {myShift : myShift});
 });
 
+app.post("/proposeSwitch", function(req,res){
+	// TODO : proposeSwitch - add logic to modify shift objects - Need body-parser
+	res.send("Proposition sent");
+})
+
 app.get("/exchange/all", function(req,res){
-	var exchanges = [
-	{number : 312, member : 98, shift: "6h-8h15", date : "15 mai 1980" },
-	{number : 315, member : 930, shift: "6h-8h15", date : "15 mai 1980" },
-	{number : 334, member : 2341, shift: "6h-8h15", date : "15 mai 1980" },
-	{number : 335, member : 21, shift: "6h-8h15", date : "16 mai 1980" },
-	{number : 336, member : 2345, shift: "6h-8h15", date : "20 mai 1980" },
-	{number : 340, member : 221, shift: "6h-8h15", date : "22 mai 1980" }
-	];
+
 	res.render('exchange_all.ejs', {exchanges:exchanges});
 })
 
@@ -46,7 +58,7 @@ app.get("/exchange/:exchangeID", function(req,res){
 	res.render('exchange_details.ejs',{exchangeIDVal : exchangeID});
 })
 
-app.get("*", function(req,res){
+app.get("/*", function(req,res){
 	console.log("request to catch all");
 	res.send("All other routes")
 })
