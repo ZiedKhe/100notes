3bf68ce8fbbc720d6a3cd930a957b330087a136b
Zied Kheriji <zied.web@gmail.com>
2017-01-20 19:16:12 +0100

    correct camprgouds page with images

--- camp/app.js
+++ camp/app.js
@@ -1,27 +1,33 @@
 const express = require ("express");
 const app = express ();
 const ejs = require ('ejs');
+const port = 3000;
 
 // RENDERING IN EJS
 
 app.set ("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));	
 
 // ROUTES
 
 app.get('/', function(req,res){
-	res.render('views/landing');
+	res.render('landing');
 });
 
 app.get('/campgrounds', function(req,res){
 
 	var campgrounds = [
-		{name : "La Fraie", image : ""},
-		{name : "Rochette", image : ""},
-		{name : "Haut Lac", image : ""}
+		{name : "La Fraie", image : "http://s3.amazonaws.com/virginiablog/wp-content/uploads/2016/03/05154213/North-Bend-Park-Campground.jpg"},
+		{name : "Rochette", image : "http://s3.amazonaws.com/virginiablog/wp-content/uploads/2016/03/05154212/CN10111903V_018.jpg"},
+		{name : "Haut Lac", image : "http://s3.amazonaws.com/virginiablog/wp-content/uploads/2016/03/05154212/Mount-Rogers-and-Whitetop-Mountain.jpg"}
 	]
-	res.render('views/campgrounds', {campgrounds : campgrounds})
+	res.render('campgrounds', {campgrounds : campgrounds})
 });
 
+
+app.post('/campgrounds', function(req,res){
+	res.send
+})
 // SERVER
 
 app.listen(port,(err)=> {
